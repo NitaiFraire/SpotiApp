4d82eb96778bd80a8d70604b87395acf99cac635
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-06-27 14:15:08 -0500

    Componente de búsqueda de artistas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,9 +14,18 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-        'Authorization': 'Bearer BQAyLoqCchSZGOKKRTbkmXSarZHwKPF9gY0vOCcsSjpXacg93NC5OMxfchXlN2NM0Fux8wmLqC1QnhsabDA'
+        'Authorization': 'Bearer BQCU6YnWN6ir4qtbOIGyln3B55FrM63gguRZEfiAJd6kktnmMF5BwtFOz67cBfCRo-UXSTzprmhJNxQceFs'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+  }
+
+  getArtista( termino: string ){
+
+    const headers = new HttpHeaders({
+        'Authorization': 'Bearer BQCU6YnWN6ir4qtbOIGyln3B55FrM63gguRZEfiAJd6kktnmMF5BwtFOz67cBfCRo-UXSTzprmhJNxQceFs'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
   }
 }
