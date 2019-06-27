0ce0da74c80062e0841002adf63222dcf3a50558
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-06-27 16:48:11 -0500

    centralizadas las peticiones hacia API Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,23 +13,27 @@ export class SpotifyService {
     console.log('spoty');
   }
 
-  getNewReleases() {
+  getQuery(query: string){
+
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-        'Authorization': 'Bearer BQC9hOaitWBPcN1Bpkcq6yQP4Esd0_pv6lW_7HapUHO4KUHs2lg-5iQUrjPOASpcGdSTyvK4PDQxi8ZhSrE'
+      'Authorization': 'Bearer BQC9hOaitWBPcN1Bpkcq6yQP4Esd0_pv6lW_7HapUHO4KUHs2lg-5iQUrjPOASpcGdSTyvK4PDQxi8ZhSrE'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-                    .pipe( map( data => data['albums'].items ));
+    return this.http.get(url, { headers })
+  }
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases')
+               .pipe( map( data => data['albums'].items ));
   }
 
   getArtista( termino: string ){
 
-    const headers = new HttpHeaders({
-        'Authorization': 'Bearer BQC9hOaitWBPcN1Bpkcq6yQP4Esd0_pv6lW_7HapUHO4KUHs2lg-5iQUrjPOASpcGdSTyvK4PDQxi8ZhSrE'
-    });
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                .pipe( map( data => data['artists'].items ));
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
-                    .pipe( map( data => data['artists'].items ));
   }
 }
